75b5d75818d68503cd753d36c944f7a06f66cf4b
jutamin <wonjy0307@naver.com>
2025-05-15 20:13:36 +0900

    Initial Commit

---
+++ LearnDot/LearnDotApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnDotApp.swift
+//  LearnDot
+//
+//  Created by 원주연 on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct LearnDotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

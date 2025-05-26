acedfd589a7d5d2670d97c8f0eeb3bec4ddaf5dc
hyeyeonie <shy020916@naver.com>
2025-05-27 01:33:11 +0900

    [feat] #10 WordCategoryView UI 구현

---
+++ LearnDot/Views/WordQuiz/WordCategoryView.swift
@@ -0,0 +1,44 @@
+//
+//  WordCategoryView.swift
+//  LearnDot
+//
+//  Created by 신혜연 on 5/26/25.
+//
+
+import SwiftUI
+
+struct WordCategoryView: View {
+    
+    let level: DifficultyLevel
+    
+    var body: some View {
+        ZStack {
+            Color.black00
+                .ignoresSafeArea()
+            
+            ScrollView(.vertical) {
+                VStack(spacing: 16) {
+                    Spacer()
+                        .frame(height: 140)
+                    
+                    ForEach(WordCategory.allCases) { category in
+                        SelectCard(
+                            title: category.rawValue,
+                            description: category.description(for: level)
+                        ) {
+                            
+                        }
+                    }
+                    Spacer()
+                }
+            }
+            .ignoresSafeArea()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .navigationTitle("카테고리 선택")
+        }
+    }
+}
+
+#Preview {
+    WordCategoryView(level: .easy)
+}

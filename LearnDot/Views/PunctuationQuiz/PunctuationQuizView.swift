dec343d6269d248afe26cf0b093d0b369ca5e391
hyeyeonie <shy020916@naver.com>
2025-06-11 17:02:09 +0900

    [feat] #17 PunctuationQuiz 문제 풀이 및 정답 확인 UI, 기능 구현

--- LearnDot/Views/PunctuationQuiz/PunctuationQuizView.swift
+++ LearnDot/Views/PunctuationQuiz/PunctuationQuizView.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct PunctuationQuizView: View {
+    @Environment(NavigationCoordinator.self) private var coordinator
+    @EnvironmentObject var viewModel: PunctuationQuizViewModel
+
+    @State private var selectedDotsArray: [[Int]] = [[]]
+    @State private var currentCellIndex: Int = 0
     
-    @State private var selectedDots: Set<Int> = []
+    let indexToDotNumber = [1, 4, 2, 5, 3, 6]
     
     var body: some View {
+        let isSubmitDisabled = selectedDotsArray.allSatisfy { $0.isEmpty }
+        
         ZStack {
             Color.black00
                 .ignoresSafeArea()
@@ -21,38 +28,51 @@ struct PunctuationQuizView: View {
                     .font(.mainTextBold24)
                     .foregroundStyle(.blue00)
                 
-                RoundedRectangle(cornerRadius: 20)
-                    .foregroundStyle(.gray06)
-                    .frame(width: 240, height: 72)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.gray, lineWidth: 1)
-                    )
-                    .overlay {
-                        Text(". 온점")
-                            .font(.mainTextSemiBold24)
-                            .foregroundStyle(.white00)
-                    }
-                    .padding(.top, 12)
+                if let quiz = viewModel.currentQuiz {
+                    RoundedRectangle(cornerRadius: 20)
+                        .foregroundStyle(.gray06)
+                        .frame(width: 240, height: 72)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                        .overlay {
+                            Text(quiz.korean)
+                                .font(.mainTextSemiBold24)
+                                .foregroundStyle(.white00)
+                        }
+                        .padding(.top, 12)
+                }
                 
                 ZStack {
                     HStack(spacing: 26) {
-                        Button {
-                            // 왼쪽 버튼 동작
-                        } label: {
-                            Image(systemName: "chevron.left")
+                        if currentCellIndex > 0 {
+                            Button {
+                                currentCellIndex -= 1
+                            } label: {
+                                Image(systemName: "chevron.left")
+                                    .frame(width: 41, height: 70)
+                                    .foregroundColor(.gray00)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 20)
+                                            .stroke(Color.blue00, lineWidth: 2)
+                                    )
+                            }
+                        } else {
+                            Color.clear
                                 .frame(width: 41, height: 70)
-                                .foregroundColor(.gray00)
-                                .background(
-                                    RoundedRectangle(cornerRadius: 20)
-                                        .stroke(Color.blue00, lineWidth: 2)
-                                )
                         }
                         
                         Spacer()
                         
                         Button {
-                            // 오른쪽 버튼 동작
+                            if !selectedDotsArray[currentCellIndex].isEmpty {
+                                currentCellIndex += 1
+                                
+                                if currentCellIndex >= selectedDotsArray.count {
+                                    selectedDotsArray.append([])
+                                }
+                            }
                         } label: {
                             Image(systemName: "chevron.right")
                                 .frame(width: 41, height: 70)
@@ -67,14 +87,15 @@ struct PunctuationQuizView: View {
                     
                     LazyVGrid(columns: [GridItem(), GridItem()], spacing: 26) {
                         ForEach(0..<6) { index in
+                            let dotNumber = indexToDotNumber[index]
                             Circle()
-                                .fill(selectedDots.contains(index) ? Color.white00 : Color.gray05)
+                                .fill(selectedDotsArray[currentCellIndex].contains(dotNumber) ? Color.white00 : Color.gray05)
                                 .frame(width: 100, height: 100)
                                 .onTapGesture {
-                                    if selectedDots.contains(index) {
-                                        selectedDots.remove(index)
+                                    if selectedDotsArray[currentCellIndex].contains(dotNumber) {
+                                        selectedDotsArray[currentCellIndex].removeAll { $0 == dotNumber }
                                     } else {
-                                        selectedDots.insert(index)
+                                        selectedDotsArray[currentCellIndex].append(dotNumber)
                                     }
                                 }
                         }
@@ -85,7 +106,8 @@ struct PunctuationQuizView: View {
                 
                 HStack(spacing: 17) {
                     Button {
-                        
+                        selectedDotsArray = [[]]
+                        currentCellIndex = 0
                     } label: {
                         RoundedRectangle(cornerRadius: 20)
                             .foregroundStyle(.gray01)
@@ -98,24 +120,28 @@ struct PunctuationQuizView: View {
                     }
                     
                     Button {
-                        
+                        let isCorrect = viewModel.isAnswerCorrect(selectedDotsArray: selectedDotsArray)
+                        coordinator.push(AppDestination.PunctuationResult(isCorrect))
                     } label: {
                         RoundedRectangle(cornerRadius: 20)
-                            .foregroundStyle(.blue01)
+                            .foregroundStyle(isSubmitDisabled ? .gray04 : .blue01)
                             .frame(width: 168, height: 64)
                             .overlay {
                                 Text("작성완료")
                                     .font(.mainTextBold24)
-                                    .foregroundStyle(.white00)
+                                    .foregroundStyle(isSubmitDisabled ? .gray02 : .white00)
                             }
                     }
+                    .disabled(isSubmitDisabled)
                 }
                 .padding(.top, 46)
             }
         }
+        .navigationBarBackButtonHidden(true)
     }
 }
 
 #Preview {
     PunctuationQuizView()
+        .environment(NavigationCoordinator())
 }
